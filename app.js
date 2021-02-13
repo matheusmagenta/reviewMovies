e42ea9cc531940a12773fc22ccdb929e45ea4178
matheusmagenta <matheus.magenta@gmail.com>
2021-02-13 00:15:29 +0000

    adding show movie details

--- app.js
+++ app.js
@@ -23,12 +23,13 @@ const state = {
   },
 };
 
+// creates movie object model
 const createMovieObject = function (data) {
   const movie = data;
   return {
     title: movie.title,
     id: movie.id,
-    year: movie.year,
+    year: movie.release_date,
     overview: movie.overview,
     poster_path: movie.poster_path,
     origin_country: movie.origin_country,
@@ -37,29 +38,31 @@ const createMovieObject = function (data) {
 };
 
 // function responsible for fetch the movie data from the TMDb api
-async function getMovie() {
-  let response = await fetch(`${API_URL}${530}?api_key=${apiKey}`);
+const getMovie = async function (id) {
+  let response = await fetch(`${API_URL}movie/${id}?api_key=${apiKey}`);
   console.log("response: ", response);
   let data = await response.json();
   console.log("json: ", data);
   state.movie = createMovieObject(data);
-}
+  await showMovie(state);
+};
 
 // show data in the view
-async function showMovie(state) {
+const showMovie = async function (state) {
   parentElement = document.querySelector(".movie");
+  clearResults();
   const markup = `
       <h1 class="movie-title">${state.movie.title}</h1>
-      <p class="movie-year">${state.movie.year}</p>
-      <p class="movie-country">${state.movie.origin_country}</p>
+      <p class="movie-year">${state.movie.year.slice(0, 4)}</p>
       <span class="vote-average">${state.movie.vote_average}</span>
       <p class="overview">
       ${state.movie.overview}
       </p>
     `;
   parentElement.insertAdjacentHTML("beforeend", markup);
-}
+};
 
+// get value from search
 function getValue() {
   const nameValue = document.getElementById(".search").getValue;
   console.log(nameValue);
@@ -88,7 +91,7 @@ const getResults = async function (querySearch) {
   );
   //console.log("response: ", response);
   let dataResults = await response.json();
-  console.log("results: ", dataResults.results);
+  //console.log("results: ", dataResults.results);
 
   dataResults.results.map((result) =>
     state.search.results.push(createResultObject(result))
@@ -99,11 +102,12 @@ const getResults = async function (querySearch) {
 const showResults = async function (state) {
   parentElement = document.querySelector(".movie");
   console.log(state);
-  state.search.results.map((result) => {
+  state.search.results.map((movie) => {
     markup = `
-  <h1 class="movie-title">${result.title}</h1>
-  <p class="movie-year">${result.year.slice(0, 4)}</p>
-  <span class="vote-average">${result.vote_average}</span>
+  <h1 class="movie-title">${movie.title}</h1>
+  <a onclick="getMovie(${movie.id})" href="#">see details</a>
+  <p class="movie-year">${movie.year.slice(0, 4)}</p>
+  <span class="vote-average">${movie.vote_average}</span>
   `;
     parentElement.insertAdjacentHTML("beforeend", markup);
   });
@@ -115,13 +119,40 @@ const showResults = async function (state) {
 
 const getSearchQuery = async function () {
   // selecting the input element and get its value
+  clearResults();
   const querySearch = document.getElementById("search").value;
+
   await getResults(querySearch);
+
+  // displaying results
   await showResults(state);
-  // displaying the value
-  //console.log(querySearch);
 };
 
 //let btnSearch = document.querySelector(".search");
 //console.log(btnSearch);
 //btnSearch.addEventListener("click", getSearchQuery);
+
+///////////
+// CLEAR //
+///////////
+
+// function to clear results
+const clearResults = async function () {
+  parentElement = document.querySelector(".movie");
+  parentElement.innerHTML = "";
+};
+
+//////////////////////////////////////////
+/// TASKS - TO BE DONE
+// create model, views and controller (MVC)
+// create back button in movie view
+// improve data model
+// fix submit input
+// fix event listeners on button
+// implement bookmark - my movies
+// implement stars feature
+// implement own review feature
+// implement user login auth feature
+// sort search results
+// sort bookmark results
+// search only in bookmark results
